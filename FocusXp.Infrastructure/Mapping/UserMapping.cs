20eb8f6c490e1a7556b13061ffd4b648bf30221a
Talha Enes Baba <34talha10@gmail.com>
2025-03-11 23:35:59 +0300

    Add UserMapping configuration with unique indexes for Username and Email

---
+++ FocusXp.Infrastructure/Mapping/UserMapping.cs
@@ -0,0 +1,24 @@
+using FocusXp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FocusXp.Infrastructure.Mapping;
+
+public class UserMapping : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(user => user.Id)
+            .ValueGeneratedNever();
+        
+        builder.HasIndex(user => user.Username)
+            .IsUnique()
+            .HasDatabaseName("users_username_unique");
+
+        builder.HasIndex(user => user.Email)
+            .IsUnique()
+            .HasDatabaseName("users_email_unique");
+
+        
+    }
+}
